462cf6ad6cfa41d4927c80e121584cb9dd51f04e
ArielFeingold <feingold.ariel@gmail.com>
2018-11-27 13:55:24 -0500

    login page design complete

--- src/assets/jss/views/loginPageStyle.jsx
+++ src/assets/jss/views/loginPageStyle.jsx
@@ -1,80 +1,23 @@
-const loginPageStyle = theme => ({
+const loginPageStyle = theme =>({
   root: {
     flexGrow: 1,
-    minHeight: "800px"
+    marginTop: "4em"
   },
   paper: {
-    margin: theme.spacing.unit * 2,
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  form: {
-    alignItems: "center"
-  },
-  header: {
-    margin: "none",
-    padding: "30px",
-    backgroundColor: "#153255",
-    borderRadius: "0px",
-    textAlign: "center !important",
-    color: "white",
-    fontSize: "2em"
-  },
-  logo: {
-    hight: "43px",
-    width: "150px",
-    display: "block"
-  },
-  migrationTool: {
-    marginTop: "30px"
-  },
-  report: {
-    marginTop: "30px",
-    marginLeft: "10px"
-  },
-  cardHeader: {
-    color: "white",
-    backgroundColor: "#FF645C",
-    hight: "20px",
-    textAlign: "center"
+    color: theme.palette.text.primary,
+    maxWidth: "500px",
+    margin: "auto"
   },
   button: {
-    marginBottom: "1em",
-    height: "50px"
-  },
-  picker: {
-    margin: "2em",
-    padding: "0.5em",
-    border: "1px solid #FF645C"
-  },
-  rightIcon: {
-    marginLeft: theme.spacing.unit * 2,
-  },
-  successBanner: {
-    margin: "none",
-    padding: "20px",
-    backgroundColor: "#51A009",
-    borderRadius: "0px",
-    textAlign: "center !important",
-    color: "white",
-    fontSize: "1em"
- },
-  noTokenBanner: {
-    margin: "none",
-    padding: "20px",
-    backgroundColor: "#FF1237",
-    borderRadius: "0px",
-    textAlign: "center !important",
-    color: "white",
-    fontSize: "1em"
- },
- input: {
-   width: "80%",
-   color: "white",
-   backgroundColor: "#FF1237",
-   border:"none"
- },
+
+    width: "50%",
+    margin: "auto",
+    marginTop: "2em",
+    height: "3em"
+  }
 });
+
 
 export default loginPageStyle;
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import classNames from 'classnames';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-
-
+import loginPageStyle from '../assets/jss/views/loginPageStyle'
 
 class LoginPage extends React.Component {
-  render(){
 
-    return(
-      <h1>LoginPage</h1>
-    )
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography variant="h4">Login</Typography>
+              <form>
+                <TextField
+                  fullWidth
+                  autoFocus
+                  helperText="Enter Username"
+                   id="username"
+                   label="Username"
+                   className={classes.textField}
+                   margin="normal"
+                   // value={this.state.name}
+                   // onChange={this.handleChange('name')}
+                   variant="outlined"
+                 />
+                 <TextField
+                   fullWidth
+                   helperText="Enter Password"
+                    id="password"
+                    label="Password"
+                    className={classes.textField}
+                    margin="normal"
+                    // value={this.state.name}
+                    // onChange={this.handleChange('name')}
+                    variant="outlined"
+                  />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    size="large">
+                    Login
+                  </Button>
+              </form>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(loginPageStyle)(LoginPage);
