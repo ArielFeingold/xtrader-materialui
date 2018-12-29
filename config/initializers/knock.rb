b3229b727f30d5e0c7746de15d28ab7e7eb56cc0
ArielFeingold <feingold.ariel@gmail.com>
2018-12-29 10:17:13 -0500

    knock set up

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,8 +1,7 @@
 Knock.setup do |config|
 
-  config.token_lifetime = 1.day
-  config.token_signature_algorithm = 'HS256'
-  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
+  # config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 
   ## Expiration claim
@@ -62,3 +61,4 @@ Knock.setup do |config|
   ## Default:
   # config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 end
+x
